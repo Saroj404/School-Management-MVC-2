0e7816c6ec16c1d089f01c65aff2bc66afbe5d78
SirOz_Shrest <Shresthasaroj901@gmail.com>
2021-06-16 14:19:55 +0545

    Changes on shared layout

--- SchoolWebAuth/Controllers/HomeController.cs
+++ SchoolWebAuth/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace SchoolWebAuth.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+           // ViewBag.Message = "Contact";
 
             return View();
         }
